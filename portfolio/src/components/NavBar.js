cc5e0b0a7902842353e06b4585dcb652d2a6f6f4
Lau Siaw Sam <lausiawsam98@gmail.com>
2021-05-18 12:03:46 +0800

    Add navbar layout

---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import styles from '../styles'
+
+const NavBar = () => {
+  return (
+    <Container>
+      <Name>
+        Lau Siaw Sam
+      </Name>
+      <NavButtons>
+        <Button>
+          EXPERIENCE
+        </Button>
+        <Button>
+          EDUCATION
+        </Button>
+        <Button>
+          PROJECTS
+        </Button>
+        <Button>
+          SKILLS
+        </Button>
+      </NavButtons>
+    </Container>
+  )
+}
+
+export default NavBar
+
+const Container = styled.div`
+  padding: 20px 30px 20px 50px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Name = styled.div`
+  display: flex;
+  align-items: center;
+
+  font-size: 30px;
+  color: ${styles.PRI_COLOR}
+`
+
+const NavButtons = styled.div`
+  display: flex;
+`
+
+const Button = styled.div`
+  padding: 10px 30px;
+
+  font-size: 14px;
+  color: ${styles.PRI_COLOR}
+`
